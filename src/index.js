e01293cc77ccfcccab36d44dc3f6305f814734c5
aboongm <aboongm@yahoo.com>
2022-12-22 10:14:11 +0530

    Change index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
+// import { Provider } from 'react-redux';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import { PersistGate } from 'redux-persist/integration/react';
+// import { persistStore } from 'redux-persist';
+import { ToastContainer } from 'react-toastify';
+// import { store } from './RtkQuery/store';
 import App from './App';
+
+// const persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <Provider store={store}> */}
+    {/* <PersistGate loading={null} persistor={persistor}> */}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+    <ToastContainer />
+    {/* </PersistGate> */}
+    {/* </Provider> */}
   </React.StrictMode>,
 );
