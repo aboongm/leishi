528656238b86cedb4cdc933295a0816a13aa6583
aboongm <aboongm@yahoo.com>
2022-12-23 20:02:01 +0530

    Fix login hook issue

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import './loginSignup.css';
-import PulseLoader from 'react-spinners/PulseLoader';
 import leishi from '../../assets/images/leishi.png';
 import { useLoginUserMutation } from '../../RtkQuery/slices/auth/authApi';
 import { setCredentials } from '../../RtkQuery/slices/auth/authSlice';
@@ -15,7 +14,7 @@ const Login = () => {
   const [password, setPassword] = useState('');
   const [errMsg, setErrMsg] = useState('');
 
-  const [loginUser, { isLoading, isSuccess }] = useLoginUserMutation();
+  const [loginUser] = useLoginUserMutation();
 
   const handleEmailInput = (e) => setEmail(e.target.value);
   const handlePasswordInput = (e) => setPassword(e.target.value);
@@ -55,78 +54,72 @@ const Login = () => {
     }
   };
 
-  let content;
+  const content = (
+    <section className="signup__container">
+      <div className="flex flex-col items-center justify-center">
+        <Link to="/">
+          <img src={leishi} alt="leishi" className="header__logo" />
+        </Link>
+        <div className="bg-white border shadow rounded lg:w-1/3 md:w-1/2 w-full p-10 mt-12">
+          <p className="text-2xl font-extrabold leading-6 login__text">
+            Login to your account
+          </p>
+          <p className="text-sm mt-4 font-medium leading-none login__text">
+            Don&apos;t have an account? &nbsp;
+            <span className="text-sm font-medium leading-none underline login__text cursor-pointer">
+              <Link to="/signup">Sign Up Here</Link>
+            </span>
+          </p>
 
-  if (isLoading) content = <PulseLoader color="#f50057" size={30} />;
+          <div className="mt-4">
+            <label
+              htmlFor="signup"
+              className="text-sm font-medium leading-none login__next"
+            >
+              <input
+                aria-label="enter you email address"
+                type="email"
+                id="email"
+                className="bg-gray-200 border rounded focus:outline-none text-base font-medium leading-none login__text py-3 w-full pl-3 mt-2"
+                value={email}
+                onChange={handleEmailInput}
+                placeholder="Email Address"
+                required
+              />
+            </label>
+          </div>
 
-  if (isSuccess) {
-    content = (
-      <section className="signup__container">
-        <div className="flex flex-col items-center justify-center">
-          <Link to="/">
-            <img src={leishi} alt="leishi" className="header__logo" />
-          </Link>
-          <div className="bg-white border shadow rounded lg:w-1/3 md:w-1/2 w-full p-10 mt-12">
-            <p className="text-2xl font-extrabold leading-6 login__text">
-              Login to your account
-            </p>
-            <p className="text-sm mt-4 font-medium leading-none login__text">
-              Don&apos;t have an account? &nbsp;
-              <span className="text-sm font-medium leading-none underline login__text cursor-pointer">
-                <Link to="/signup">Sign Up Here</Link>
-              </span>
-            </p>
-
-            <div className="mt-4">
-              <label
-                htmlFor="signup"
-                className="text-sm font-medium leading-none login__next"
-              >
-                <input
-                  aria-label="enter you email address"
-                  type="email"
-                  id="email"
-                  className="bg-gray-200 border rounded focus:outline-none text-base font-medium leading-none login__text py-3 w-full pl-3 mt-2"
-                  value={email}
-                  onChange={handleEmailInput}
-                  placeholder="Email Address"
-                  required
-                />
-              </label>
-            </div>
-
-            <div className="mt-4">
-              <label
-                htmlFor="signup"
-                className="text-sm font-medium leading-none login__next"
-              >
-                <input
-                  aria-label="enter you email address"
-                  type="password"
-                  id="password"
-                  className="bg-gray-200 border rounded focus:outline-none text-base font-medium leading-none login__text py-3 w-full pl-3 mt-2"
-                  value={password}
-                  onChange={handlePasswordInput}
-                  placeholder="Password"
-                  required
-                />
-              </label>
-            </div>
-            <div className="mt-8">
-              <button
-                onClick={handleSubmit}
-                type="button"
-                aria-label="create my account"
-                className="signup__button"
-              >
-                Login
-              </button>
-            </div>
+          <div className="mt-4">
+            <label
+              htmlFor="signup"
+              className="text-sm font-medium leading-none login__next"
+            >
+              <input
+                aria-label="enter you email address"
+                type="password"
+                id="password"
+                className="bg-gray-200 border rounded focus:outline-none text-base font-medium leading-none login__text py-3 w-full pl-3 mt-2"
+                value={password}
+                onChange={handlePasswordInput}
+                placeholder="Password"
+                required
+              />
+            </label>
+          </div>
+          <div className="mt-8">
+            <button
+              onClick={handleSubmit}
+              type="button"
+              aria-label="create my account"
+              className="signup__button"
+            >
+              Login
+            </button>
           </div>
         </div>
-      </section>
-    );
-  }
+      </div>
+    </section>
+  );
 
   return content;
 };
