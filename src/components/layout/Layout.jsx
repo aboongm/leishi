ea4632e7d59b27444a539b303582d10adc0ee583
aboongm <aboongm@yahoo.com>
2022-12-24 08:54:45 +0530

    Fix stylelint errors

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,14 +2,11 @@ import { Outlet } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 
-const Layout = () => {
-  return (
-    <>
-      <Header />
-      <Outlet />
-      <Footer />
-    </>
-  );
-};
-
+const Layout = () => (
+  <>
+    <Header />
+    <Outlet />
+    <Footer />
+  </>
+);
 export default Layout;
