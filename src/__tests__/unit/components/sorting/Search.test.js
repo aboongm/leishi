8ac2d37b97d1884dd3705247f3f172dfd4b1d1c6
aboongm <aboongm@yahoo.com>
2023-02-08 17:26:44 +0530

    Add unit test Search.test.js

---
+++ src/__tests__/unit/components/sorting/Search.test.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import Search from '../../../../components/sorting/Search';
+
+const state = {
+  search: {
+    search: [
+      {
+        id: 1,
+        title: 'Test Product 1',
+        image: 'test_image_1.jpg',
+        price: 100,
+        rating: 4,
+      },
+      {
+        id: 2,
+        title: 'Test Product 2',
+        image: 'test_image_2.jpg',
+        price: 200,
+        rating: 5,
+      },
+    ],
+  },
+};
+
+const store = createStore(() => state);
+
+describe('Search component', () => {
+  test('renders search results correctly', () => {
+    render(
+      <Provider store={store}>
+        <Search />
+      </Provider>
+    );
+
+    expect(screen.getByText('Search Results')).toBeTruthy();
+    expect(screen.getByText('Test Product 1')).toBeTruthy();
+    expect(screen.getByText('Test Product 2')).toBeTruthy();
+  });
+});
